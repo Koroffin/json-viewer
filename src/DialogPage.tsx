fa4d6947c68e5bd0db9e8674cf5e7dbf478382f1
Nikita Korovin <n.koroffin@gmail.com>
2023-10-21 03:28:28 +0400

    grid & shadow

--- src/DialogPage.tsx
+++ src/DialogPage.tsx
@@ -6,7 +6,7 @@ export const DialogPage = () => {
   return (
     <DndProvider backend={HTML5Backend}>
       <DialogContainer />
-      <div style={{ position: "fixed", left: 0, right: 0, bottom: 0 }}>
+      <div style={{ boxShadow: "0 0 10px rgba(0, 0, 0, 0.5)", position: "fixed", left: 0, right: 0, bottom: 0, backgroundColor: "white" }}>
         <DialogForm />
       </div>
       <DialogSaveButton />
