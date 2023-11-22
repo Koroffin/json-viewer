de93ea3874d9283b4d1ec762d21776b7ad382d89
Nikita Korovin <n.koroffin@gmail.com>
2023-11-22 20:56:45 +0400

    release with fixes

--- src/DialogPage.tsx
+++ src/DialogPage.tsx
@@ -1,14 +1,24 @@
+import styled from '@emotion/styled';
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { DialogContainer, DialogForm, DialogSaveButton } from "@arbuzalchemy/dialog-tree";
+
+const BottomBar = styled.div`
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: white;
+`;
 
 export const DialogPage = () => {
   return (
     <DndProvider backend={HTML5Backend}>
       <DialogContainer />
-      <div style={{ boxShadow: "0 0 10px rgba(0, 0, 0, 0.5)", position: "fixed", left: 0, right: 0, bottom: 0, backgroundColor: "white" }}>
+      <BottomBar>
         <DialogForm />
-      </div>
+      </BottomBar>
       <DialogSaveButton />
     </DndProvider>
   );
